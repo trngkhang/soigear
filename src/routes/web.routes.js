0ffc691556c1eb0d71c1109514b290492e971b8d
trngkhang <130826168+trngkhang@users.noreply.github.com>
2024-02-11 14:01:34 +0700

    refactor:config controllers

--- src/routes/web.routes.js
+++ src/routes/web.routes.js
@@ -1,7 +1,6 @@
 const express = require("express");
+const getHome = require("../controllers/home.controllers");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("home");
-});
+router.get("/", getHome);
 module.exports = router;

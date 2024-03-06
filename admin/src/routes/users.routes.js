08ce9a6f8ce1b61f5fe10b9d2e24a31f713b9c1b
trngkhang <130826168+trngkhang@users.noreply.github.com>
2024-03-06 23:33:25 +0700

    feat: admin edit user

--- admin/src/routes/users.routes.js
+++ admin/src/routes/users.routes.js
@@ -4,9 +4,13 @@ const {
   getAllUsers,
   getCreateUser,
   postCreateUser,
+  getEditUser,
+  postEditUser,
 } = require("../controllers/users.controllers");
 
 router.get("/all", getAllUsers);
 router.get("/create", getCreateUser);
 router.post("/create", postCreateUser);
+router.get("/:userId/edit", getEditUser);
+router.post("/:userId/edit", postEditUser);
 module.exports = router;
--- admin/src/services/adminServices.js
+++ admin/src/services/adminServices.js
@@ -12,7 +12,25 @@ const createUser = async (user) => {
     [user.email, user.password, user.name, user.address]
   );
 };
+const sv_readUser = async (id) => {
+  const [results] = await connection.query(
+    `select user_id, email, password , name , address from users where user_id=?`,
+    [id]
+  );
+  return results;
+};
+const sv_editUser = async (user) => {
+  console.log(user);
+  const results = await connection.query(
+    `update Users
+    set password=?, name=?, address=?
+    where user_id=?`,
+    [user.password, user.name, user.address, user.user_id]
+  );
+};
 module.exports = {
   allUsers,
   createUser,
+  sv_editUser,
+  sv_readUser,
 };
